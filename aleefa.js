553619807958b3e9bb6227e555f367e1e3ff2a94
imthecode777 <rahilas272@gmail.com>
2024-10-20 21:44:01 +0530

    first commit

---
+++ aleefa.js
@@ -0,0 +1,31 @@
+function showMessage() {
+  document.getElementById("message").classList.add("show");
+  var duration = 10 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 45, spread: 360, ticks: 80, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 100 * (timeLeft / duration);
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: {
+          x: randomInRange(0.1, 0.9),
+          y: Math.random() - 0.2,
+        },
+        colors: ["#ff69b4", "#ff99cc", "#ffb6c1", "#ffe5ec", "#ff1493"],
+        gravity: randomInRange(0.4, 0.7),
+        scalar: randomInRange(0.8, 1.5),
+      })
+    );
+  }, 150);
+}
